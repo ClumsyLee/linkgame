19e13e4c91f0cc8ae75bce47bffa7fb2661cca00
Thomas Lee <lisihan969@gmail.com>
2015-08-09 17:10:24 +0800

    Accpet all close matches.

--- src/map_game.m
+++ src/map_game.m
@@ -6,25 +6,17 @@ function [ground, legends] = map_game(imgs, matches, values, ...
     kind_num = 1;
 
     % Accpet all close matches.
-    % last_close = find(values < accept_threshold) - 1;
-    last_close = length(matches);
+    last_close = find(values < accept_threshold) - 1;
     for k = 1:last_close
         match = matches(k, :);
         kinds = ground(match);
         if kinds == 0  % New kind.
-            figure
-            subplot 121
-            imshow(imgs{match(1)});
-            title(kind_num);
-            subplot 122
-            imshow(imgs{match(2)});
-
             ground(match) = kind_num;
             legends{kind_num} = imgs{match(1)};
             kind_num = kind_num + 1;
-        % elseif any(kinds == 0)  % Old kind, one not classified.
-        %     ground(match) = max(kinds);
-        % elseif kinds(1) ~= kinds(2)  % Old kind & close, combine.
-        %     ground(ground == kinds(2)) = kinds(1);
+        elseif any(kinds == 0)  % Old kind, one not classified.
+            ground(match) = max(kinds);
+        elseif kinds(1) ~= kinds(2)  % Old kind & close, combine.
+            ground(ground == kinds(2)) = kinds(1);
         end  % Else already the same.
     end
